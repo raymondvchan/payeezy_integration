1803226a452a3d7a504f0d13c25fb6993e728515
raymondvchan <39813003+raymondvchan@users.noreply.github.com>
2021-07-06 10:23:44 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Payeezy Credentials for Gateway transaction downloads
+# Note: due to how Payeezy is configured, Payeezy requires passwords to be reset every 60 days. You will need to update password below
+userid = 'user'
+password = 'pass'
+
+
+# Script Options
+search_interval = 1440 # minutes
+
+
+# Payeezy gateway search url
+url = 'https://api.globalgatewaye4.firstdata.com/transaction/search'
